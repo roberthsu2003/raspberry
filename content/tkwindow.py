8399916673577fdc24b59f8435dcb832eaefdd35
roberthsu2003 <roberthsu2003@gmail.com>
2020-12-26 14:50:39 +0800

    modify

--- content/tkwindow.py
+++ content/tkwindow.py
@@ -1,1 +1,13 @@
-print("Hello! World")
+import tkinter as tk
+
+class Window(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Hello Tkinter")
+        
+        label = tk.Label(self, text="Hello World!")
+        label.pack(fill=tk.BOTH, expand=True, padx=100, pady=50)
+    
+if __name__ == "__main__":
+    window = Window()
+    window.mainloop()
