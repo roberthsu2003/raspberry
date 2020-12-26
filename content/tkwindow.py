094b13d5cb5cf10b9a3e18814bf074067a0f2f96
roberthsu2003 <roberthsu2003@gmail.com>
2020-12-26 16:00:47 +0800

    modify

--- content/tkwindow.py
+++ content/tkwindow.py
@@ -9,6 +9,12 @@ class Window(Tk):
         
         label = Label(self, text="Hello World!")
         label.pack(fill=BOTH, expand=True, padx=100, pady=50)
+        
+        hello_button = Button(self,text="Say Hello")
+        hello_button.pack(side=LEFT, padx=(20,0), pady=(0, 20))
+        
+        goodbye_button = Button(self,text="Say Goodbye")
+        goodbye_button.pack(side=RIGHT, padx=(0, 20), pady=(0, 20))
     
 if __name__ == "__main__":
     window = Window()
