958faaba599a892ab29b4416e9cd2d783c13232f
roberthsu2003 <roberthsu2003@gmail.com>
2021-01-09 09:57:12 +0800

    modify

--- content/ledControl.py
+++ content/ledControl.py
@@ -1,8 +1,12 @@
 #! usr/bin/python3
 import RPi.GPIO as GPIO
+from tkinter import *
 
 if __name__ == '__main__':
     GPIO.setmode(GPIO.BCM)
     GPIO.cleanup()
     GPIO.setwarnings(False)
     GPIO.setup(25, GPIO.OUT)
+
+    window = Tk()
+    window.mainloop()
