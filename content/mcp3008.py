0f10cd38a4226adef1c27538f13144eb55a8c023
roberthsu2003 <roberthsu2003@gmail.com>
2021-01-16 16:10:13 +0800

    modify

--- content/mcp3008.py
+++ content/mcp3008.py
@@ -2,8 +2,11 @@ from gpiozero import MCP3008
 import time
 
 lm35 = MCP3008(6)
+lightness = MCP3008(7)
 
 while True:
     value = lm35.value
-    print(value*3.3*100)
+    print('溫度:',value*3.3*100)
+    value = lightness.value
+    print('明亮度:',value*100)
     time.sleep(1)
