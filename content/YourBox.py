aa50e8d2039679dbe1d40bfea04070f7bd09dc21
roberthsu2003 <roberthsu2003@gmail.com>
2021-01-23 14:50:54 +0800

    modify

--- content/YourBox.py
+++ content/YourBox.py
@@ -10,6 +10,11 @@ class Linebox():
 
         #設定介面
         mainFrame = Frame(w,borderwidth=2,relief=GROOVE,padx=100,pady=10)
-        Label(mainFrame, text="室內溫度:").pack()
+        Label(mainFrame, text="室內溫度:").grid(row=0, column=0, sticky=E, padx=5, pady=20)
+        Label(mainFrame, text="室內光線:").grid(row=1, column=0, sticky=E, padx=5, pady=20)
+        Label(mainFrame, text="可變電阻:").grid(row=2, column=0, sticky=E, padx=5, pady=20)
+        Label(mainFrame, text="10").grid(row=0, column=1, sticky=E, padx=5, pady=20)
+        Label(mainFrame, text="89").grid(row=1, column=1, sticky=E, padx=5, pady=20)
+        Label(mainFrame, text="75").grid(row=2, column=1, sticky=E, padx=5, pady=20)
         mainFrame.pack(padx=10, pady=10)
         
