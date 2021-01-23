dceaa694ca1ab9a1fadac860af1b9bc49746fe25
roberthsu2003 <roberthsu2003@gmail.com>
2021-01-23 15:06:48 +0800

    modify

--- content/YourBox.py
+++ content/YourBox.py
@@ -8,13 +8,23 @@ class Linebox():
         w.option_add("*background", "#068587")
         w.option_add("*foreground", "#ffffff")
 
+        #設定介面變數
+        self.temperatureText = StringVar()
+        self.lightnessText = StringVar()
+        self.mvariable = StringVar()
+
         #設定介面
         mainFrame = Frame(w,borderwidth=2,relief=GROOVE,padx=100,pady=10)
         Label(mainFrame, text="室內溫度:").grid(row=0, column=0, sticky=E, padx=5, pady=20)
         Label(mainFrame, text="室內光線:").grid(row=1, column=0, sticky=E, padx=5, pady=20)
         Label(mainFrame, text="可變電阻:").grid(row=2, column=0, sticky=E, padx=5, pady=20)
-        Label(mainFrame, text="10").grid(row=0, column=1, sticky=E, padx=5, pady=20)
-        Label(mainFrame, text="89").grid(row=1, column=1, sticky=E, padx=5, pady=20)
-        Label(mainFrame, text="75").grid(row=2, column=1, sticky=E, padx=5, pady=20)
+        Label(mainFrame, textvariable=self.temperatureText).grid(row=0, column=1, sticky=E, padx=5, pady=20)
+        Label(mainFrame, textvariable=self.lightnessText).grid(row=1, column=1, sticky=E, padx=5, pady=20)
+        Label(mainFrame, textvariable=self.mvariable).grid(row=2, column=1, sticky=E, padx=5, pady=20)
         mainFrame.pack(padx=10, pady=10)
-        
+
+        #改變介面變數值
+
+        self.temperatureText.set("100")
+        self.lightnessText.set("90")
+        self.mvariable.set("80")
