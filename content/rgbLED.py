f5346c192ae197008282694b1d194f86cd3815a5
roberthsu2003 <roberthsu2003@gmail.com>
2021-01-23 12:03:32 +0800

    modify

--- content/rgbLED.py
+++ content/rgbLED.py
@@ -1,6 +1,7 @@
 from gpiozero import RGBLED, Button, MCP3008
 from signal import pause
 from random import random
+from threading import Timer
 
 def user_press():
     print("user Press")
@@ -13,13 +14,18 @@ def user_release():
     print("user Release")
     led.color = (0,0,0)
 
+def system_repeat():
+    mValue = m1.value
+    print('可變電阻值',mValue)
+    led.color = (0,0,mValue)
+    Timer(0.1,system_repeat).start()
+
 if __name__ == '__main__':
     button = Button(pin=18)
     led = RGBLED(red=17, green=27, blue=22)
-    m1 = MCP3008(0)
-    print(m1.value)
+    m1 = MCP3008(0)    
+    system_repeat()
+    
     button.when_pressed = user_press
     button.when_released = user_release
-
-
 pause()
