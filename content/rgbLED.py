f9547f7db23d455a122dcec70770b46fd21a0973
roberthsu2003 <roberthsu2003@gmail.com>
2021-01-23 10:26:38 +0800

    modify

--- content/rgbLED.py
+++ content/rgbLED.py
@@ -3,9 +3,17 @@ from signal import pause
 
 def user_press():
     print("user Press")
+    led.color = (1,0,0)
+
+def user_release():
+    print("user Release")
+    led.color = (0,0,0)
 
 if __name__ == '__main__':
     button = Button(pin=18)
+    led = RGBLED(red=17, green=27, blue=22)
     button.when_pressed = user_press
+    button.when_released = user_release
+
 
 pause()
