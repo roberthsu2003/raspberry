6c226483f9905c4c03ef1a73951b1649b4c11e01
roberthsu2003 <roberthsu2003@gmail.com>
2021-01-30 10:33:15 +0800

    modify

--- content/sendLine.py
+++ content/sendLine.py
@@ -1,6 +1,13 @@
 import requests
 import time
-
+header = {'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36'}
 while True:
-    print("Hello!")
+    paras = {'value1':'50',
+             'value2':'60'   
+            }
+    response = requests.get("https://maker.ifttt.com/trigger/over30/with/key/eDqcZfqY_i_BHCZVXCwb6aq7GLPKpdV4q1ePja35Mjq",
+                params = paras,
+                headers = header
+               )
+    print(response.status_code)
     time.sleep(60)
