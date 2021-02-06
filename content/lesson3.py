71130efc8f0d7dff3ef06df055e80a28ec658a26
roberthsu2003 <roberthsu2003@gmail.com>
2021-02-06 10:46:08 +0800

    modify

--- content/lesson3.py
+++ content/lesson3.py
@@ -1,5 +1,15 @@
 from raspigpio.lcd_display import lcd
+from gpiozero import Buzzer
+from time import sleep
 
 my_lcd = lcd()
 my_lcd.display_string("Raspberry Pi", 1)
 my_lcd.display_string("Hello!", 2)
+my_buzzer = Buzzer(16)
+
+while True:
+    sleep(10)
+    my_buzzer.on()
+    sleep(0.2)
+    my_buzzer.off()
+    
