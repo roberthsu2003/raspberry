040503a4af08471789078bc41542b99bc0e87516
roberthsu2003 <roberthsu2003@gmail.com>
2021-02-06 14:05:01 +0800

    modify

--- content/lesson5.py
+++ content/lesson5.py
@@ -1,11 +1,24 @@
 import RPi.GPIO as GPIO
+from raspigpio.lcd_display import lcd
 from tkinter import *
+import mfrc522 as MFRC522
+import threading
 
 class App():
     def __init__(self,window):
-        pass
+        #初始化lcd
+        self.my_lcd = lcd()
+
+        #初始化RFID
+        self.MIFAREReader= MFRC522.MFRC522()
+        self.rfidStatusHandler()
+    
+    def rfidStatusHandler(self):
+        print('偵測rfid')
+        threading.Timer(1, self.rfidStatusHandler).start()
 
 def on_closing():
+    GPIO.cleanup()
     root.destroy()
 
 if __name__ == "__main__":
