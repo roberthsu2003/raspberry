ccb5327e30b0b63c8c49080d9304be2d8c156097
Robert Hsu <roberthsu2003@gmail.com>
2021-02-27 13:49:06 +0800

    Update face1.py

--- content/face1.py
+++ content/face1.py
@@ -1,1 +1,14 @@
-Hello
+import face_recognition
+import picamera
+import numpy as np
+from tkinter import *
+
+class App:
+    pass;
+
+if __name__ == '__main__':
+    window = Tk()
+    window.title("Camera")
+    window.configure(background='lightgray')
+    app = App()
+    window.mainloop()
