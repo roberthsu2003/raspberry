ded51be93471be6ce84130d5a73ab01800bc0377
Robert Hsu <roberthsu2003@gmail.com>
2021-02-27 14:07:32 +0800

    Update face1.py

--- content/face1.py
+++ content/face1.py
@@ -4,11 +4,18 @@ import numpy as np
 from tkinter import *
 
 class App:
-    pass;
+    
+    def __init__(self,window):
+        myFont = font.Font(family='Helvetica',size=20)
+        btn = Button(window,text='臉部辨識',font=myFont,command=self.buttonClick).pack(expand=YES,fill=BOTH,padx=10,pady=10)
+    
+    def buttonClick(self):
+        print("userClick")
 
 if __name__ == '__main__':
     window = Tk()
     window.title("Camera")
     window.configure(background='lightgray')
-    app = App()
+    window.geometry("320x200")
+    app = App(window)
     window.mainloop()
