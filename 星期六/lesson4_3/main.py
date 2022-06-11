9f73ca857f183b30bbdfc7987daa9069f11d5c0a
robertHsu <roberthsu2003@gmail.com>
2022-06-11 13:58:00 +0800

    modify

--- 星期六/lesson4_3/main.py
+++ 星期六/lesson4_3/main.py
@@ -8,5 +8,8 @@ class Window(tk.Tk):
 
 if __name__ == "__main__":
     window = Window()
-    window.title("連線iftttt")   
+    window.title("連線iftttt")
+    window.geometry("400x200+300+300")
+    window.resizable(width=False,height=False)
+    window.configure(background="#333333")
     window.mainloop()
