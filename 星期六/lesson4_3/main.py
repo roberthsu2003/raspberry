ee60dc5c1cb616fafa58ea5b90549cccc1126221
robertHsu <roberthsu2003@gmail.com>
2022-06-11 13:43:26 +0800

    modify

--- 星期六/lesson4_3/main.py
+++ 星期六/lesson4_3/main.py
@@ -1,6 +1,12 @@
 import tkinter as tk
 
+class Window(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        
+
+
 if __name__ == "__main__":
-    window = tk.Tk()
-    window.title("這是我的第一個視窗")
+    window = Window()
+    window.title("連線iftttt")   
     window.mainloop()
