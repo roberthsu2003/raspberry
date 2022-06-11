06631f53af285fd391d35c8669514670f23a8537
robertHsu <roberthsu2003@gmail.com>
2022-06-11 09:40:53 +0800

    modify

---
+++ 星期六/lesson4_1/main.py
@@ -0,0 +1,5 @@
+import tools
+
+if __name__ == "__main__":
+    print("主程式")
+    print(f"pi={tools.PI}")
---
+++ 星期六/lesson4_1/tools.py
@@ -0,0 +1,8 @@
+#moudle
+#namespace 名字tools
+#放常數
+#放class
+#放function
+
+print(f"tools.py的namespace:{__name__}")
+PI = 3.14159
