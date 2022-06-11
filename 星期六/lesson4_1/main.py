95c3c1c2a6044efdf3e08448e46f4cba805073a5
robertHsu <roberthsu2003@gmail.com>
2022-06-11 09:56:25 +0800

    modify

--- 星期六/lesson4_1/main.py
+++ 星期六/lesson4_1/main.py
@@ -1,5 +1,12 @@
 import tools
 
-if __name__ == "__main__":
+def main():
+    mainVar = 20 #區域變數  
     print("主程式")
     print(f"pi={tools.PI}")
+    print(f"mainVar={mainVar}")
+
+if __name__ == "__main__":
+    mainVar = 10 #全域變數
+    main()
+    
