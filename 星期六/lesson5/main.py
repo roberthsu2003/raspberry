3f0c9e45276c8acb1bb90686ac118be7a81c9a0d
robertHsu <roberthsu2003@gmail.com>
2022-06-18 13:40:43 +0800

    modify

--- 星期六/lesson5/main.py
+++ 星期六/lesson5/main.py
@@ -2,6 +2,7 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 import tkinter as tk
+import tk_tools
 
 cred = credentials.Certificate('firebase_key/raspberryfirebase-firebase-adminsdk-y4f0x-e21c25a365.json')
 
@@ -15,6 +16,9 @@ print(ref.get())
 class Window(tk.Tk):
     def __init__(self):
         super().__init__()
+        led = tk_tools.Led(self, size=50)
+        led.to_green()
+        led.pack()
 
 
 if __name__ == "__main__":
