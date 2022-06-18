0e398cd049aed3bc8a95ce180e16c1672f898799
robertHsu <roberthsu2003@gmail.com>
2022-06-18 14:25:35 +0800

    modify

--- 星期六/lesson5/main.py
+++ 星期六/lesson5/main.py
@@ -10,7 +10,7 @@ firebase_admin.initialize_app(cred,{
     'databaseURL':'https://raspberryfirebase.firebaseio.com/'
 })
 
-ref = db.reference('raspberry2022/led')
+ref = db.reference('raspberry2022')
 
 
 class Window(tk.Tk):
@@ -18,8 +18,8 @@ class Window(tk.Tk):
         super().__init__()        
         self.led = tk_tools.Led(self, size=50)       
         self.led.pack()
-        tk.Button(self, text="SWITCH",command=self.switch_light).pack(padx=5, pady=8)
-        self.switch_light()
+        tk.Button(self, text="SWITCH",command=self.buttonClick).pack(padx=5, pady=8)
+        
 
     def switch_light(self):
         led_state =  ref.get()
@@ -27,6 +27,10 @@ class Window(tk.Tk):
             self.led.to_grey()
         else:
             self.led.to_green()
+
+    def buttonClick(self):
+        value  =  not ref.get()['led']
+        ref.update({'led':value})
 
 
 if __name__ == "__main__":
