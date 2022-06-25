af5f4c2fbaf826f728c915faac3ca9fd55a8ccb4
robertHsu <roberthsu2003@gmail.com>
2022-06-25 11:18:04 +0800

    modify

--- 星期六/lesson3.py
+++ 星期六/lesson3.py
@@ -5,37 +5,63 @@ from firebase_admin.exceptions import FirebaseError
 
 import tkinter as tk
 
-cred = credentials.Certificate("firebase_key/raspberryfirebase-firebase-adminsdk-y4f0x-e21c25a365.json")
+class Window(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        #---------------Firebase realtime database 初始化
+        cred = credentials.Certificate("firebase_key/raspberryfirebase-firebase-adminsdk-y4f0x-e21c25a365.json")
 
-firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://raspberryfirebase.firebaseio.com/'
-})
+        firebase_admin.initialize_app(cred, {
+            'databaseURL': 'https://raspberryfirebase.firebaseio.com/'
+        })
 
+        self.ref = db.reference('raspberrypi/Radiobutton')
+        
 
-root = tk.Tk()
-root.title('Radiobutton')
-ref = db.reference('raspberrypi/Radiobutton')
-var = tk.IntVar()
-def colorChanged(event):
-    if (type(event.data) is dict):
-        print(event.data['color'])
-        var.set(event.data['color'])
-    else:
-        print(event.data)
-        var.set(event.data)
+        #-----------建立tkinter----------------------
 
-try:
-    ref.listen(colorChanged)
-except FirebaseError as e:
-    print(e)
-
-def getEvent(_):
-    ref.set({
-        'color':var.get()
-    })
+        self.radio_item_value = tk.IntVar()
+        self.title("python視窗和Firebase及時資料庫")
+        mainFrame  = tk.Frame(self, relief="groove", borderwidth=2)
+        titleFrame = tk.Frame(mainFrame)
+        tk.Label(titleFrame,text="python視窗和Firebase及時資料庫",font=("Arial",20,'bold'),fg="#555555").pack(padx=10)
+        titleFrame.pack(pady=30)
+        mainFrame.pack(pady=30,padx=30,ipadx=30,ipady=30)
     
+        #-------------建立inputFrame-------------------
+        inputFrame = tk.Frame(mainFrame,width=50)
+        red=tk.Radiobutton(inputFrame, text='紅燈', value=1, variable=self.radio_item_value,command=self.getEvent,font=("Arial",15)).pack(side=tk.LEFT)
+        
+        green=tk.Radiobutton(inputFrame, text='綠燈', value=2, variable=self.radio_item_value,command=self.getEvent,font=("Arial",15)).pack(side=tk.LEFT)
+        blue=tk.Radiobutton(inputFrame, text='藍燈', value=3, variable=self.radio_item_value,command=self.getEvent,font=("Arial",15)).pack(side=tk.LEFT)
+        inputFrame.pack()
 
-red=tk.Radiobutton(root, text='紅燈', value=1, variable=var,command=lambda:getEvent(red)).pack(anchor=tk.W)
-green=tk.Radiobutton(root, text='綠燈', value=2, variable=var,command=lambda:getEvent(green)).pack(anchor=tk.W)
-blue=tk.Radiobutton(root, text='藍燈', value=3, variable=var,command=lambda:getEvent(blue)).pack(anchor=tk.W)
-root.mainloop()
+        #註冊完後,會立刻執行一次，所以放在最後面比較好
+        try:
+            self.ref.listen(self.colorChanged)
+        except FirebaseError as e:
+            print(e)
+
+    def getEvent(self):        
+        self.ref.set({
+            'color':self.radio_item_value.get()
+        })
+
+    def colorChanged(self,event):
+        #print(event.path)
+        #print(event.data)
+        self.radio_item_value.set(event.data['color'])
+
+        
+
+
+def closeWindow():
+    print("close window")
+    window.destroy()
+
+if __name__ == "__main__":
+    window = Window()
+    window.resizable(width=0,height=0)
+    window.protocol("WM_DELETE_WINDOW",closeWindow)
+    window.mainloop()
+
