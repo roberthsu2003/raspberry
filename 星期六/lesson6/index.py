b2b2c0a8f8ad4872993de3786999fb21604b6239
robertHsu <roberthsu2003@gmail.com>
2022-07-02 10:09:33 +0800

    modify

--- 星期六/lesson6/index.py
+++ 星期六/lesson6/index.py
@@ -1,7 +1,13 @@
 import tkinter as tk
 
 class Window(tk.Tk):
-    pass
+    def __init__(self):
+        super().__init__()
+        mainFrame = tk.Frame(self, relief="groove", borderwidth=2)
+        titleFrame = tk.Frame(mainFrame)
+        tk.Label(titleFrame,text="python視窗和Firebase及時資料庫_RGBLED",font=("Arial",20),fg="#555555").pack(padx=10)
+        titleFrame.pack(pady=30)
+        mainFrame.pack(padx=30, pady=30, ipadx=30, ipady=30)
 
 def closeWindow():
     print("close window")
@@ -9,6 +15,7 @@ def closeWindow():
 
 if __name__ == "__main__":
     window = Window()
+    window.title("python視窗和Firebase及時資料庫")
     window.resizable(width=0, height=0)
     window.protocol("WM_DELETE_WINDOW",closeWindow)
     window.mainloop()
