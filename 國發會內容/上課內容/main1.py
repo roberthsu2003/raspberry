890947a6af059bbd89ebe2f64145625f55327bb8
roberthsu2003 <roberthsu2003@gmail.com>
2022-10-06 14:17:37 +0800

    modify

---
+++ 國發會內容/上課內容/main1.py
@@ -0,0 +1,10 @@
+import tkinter as tk 
+
+def main():
+    window = tk.Tk()
+    window.title("first window")
+    window.geometry("800x300")
+    window.mainloop()
+
+if __name__ == "__main__":
+    main()
