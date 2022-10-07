796ba21f6c83bb750ba7534fef87bd256a052780
徐國堂 <roberthsu2003@gmail.com>
2022-10-07 10:29:18 +0800

    modify

--- 國發會內容/上課內容/main1.py
+++ 國發會內容/上課內容/main1.py
@@ -1,12 +1,15 @@
 import tkinter as tk 
 
+def user_click():
+    print("使用者按下")
+
 def main():
     window = tk.Tk()
     window.title("first window")
     #window.geometry("800x300")
-    btn = tk.Button(window,text="press me",padx=30,pady=20,font=('arial',20))
+    btn = tk.Button(window,text="請按我",padx=30,pady=20,font=('arial',20),command=user_click)
     btn.pack(padx=100,pady=100)
-    
+
     window.mainloop()
 
 if __name__ == "__main__":
