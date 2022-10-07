345e7b53e81abcfadf140ad72e606b1a1b374b7b
徐國堂 <roberthsu2003@gmail.com>
2022-10-07 14:12:37 +0800

    modify

--- 國發會內容/上課內容/main1.py
+++ 國發會內容/上課內容/main1.py
@@ -2,6 +2,10 @@ import tkinter as tk
 import RPi.GPIO as GPIO
 
 
+def on_close():
+    GPIO.output(25, GPIO.LOW)
+    GPIO.cleanup()
+    window.destroy()
 
 def user_click():
     global ledState
@@ -24,8 +28,10 @@ def main():
 
     #視窗
     global btn
+    global window
     window = tk.Tk()
-    window.title("first window")
+    window.title("LED Control")
+    window.protocol("WM_DELETE_WINDOW",on_close)
     #window.geometry("800x300")
     btn = tk.Button(window,text="開燈",padx=30,pady=20,font=('arial',20),command=user_click)
     btn.pack(padx=100,pady=100)
@@ -35,4 +41,5 @@ def main():
 if __name__ == "__main__":
     ledState = False
     btn = None
+    window = None
     main()
