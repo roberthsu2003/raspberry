86ceee01fbba95d1430aab23776a66e4217e51ff
徐國堂 <robert@gmail.com>
2022-10-17 14:39:50 +0800

    建立按鈕和title

--- _111_03_all/專案1_LedControl/main.py
+++ _111_03_all/專案1_LedControl/main.py
@@ -3,6 +3,11 @@ import tkinter as tk
 class Window(tk.Tk):
     def __init__(self):
         super().__init__()
+        #建立title
+        self.title("LED Controller")
+        #建立按鈕
+        btn = tk.Button(self,text="開關",padx=50,pady=30,font=('arial',18))
+        btn.pack(padx=50,pady=30)
 
 def main():
     window = Window()
