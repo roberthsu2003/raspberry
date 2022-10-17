0b74679586f17af956fe3c608800f7e5c1b1d4be
徐國堂 <robert@gmail.com>
2022-10-17 13:54:24 +0800

    建立預設的window

--- _111_03_all/專案1_LedControl/main.py
+++ _111_03_all/專案1_LedControl/main.py
@@ -1,5 +1,12 @@
+import tkinter as tk
+
+class Window(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
 def main():
-    pass
+    window = Window()
+    window.mainloop()
 
 if __name__ == "__main__":
     main()
