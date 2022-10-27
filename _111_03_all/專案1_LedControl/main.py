9519d4e9f33fca3a7cc50b5db70635284dde577f
roberthsu2003 <roberthsu2003@gmail.com>
2022-10-27 10:45:58 +0800

    新增開和關的按鈕

--- _111_03_all/專案1_LedControl/main.py
+++ _111_03_all/專案1_LedControl/main.py
@@ -16,24 +16,31 @@ class Window(tk.Tk):
         super().__init__()
         #建立title
         self.title("LED Controller")
+
         #建立按鈕
+
+        ##建立圖片
         close_image = Image.open('light_close.png')
         self.close_photo = ImageTk.PhotoImage(close_image)
+
+        open_image = Image.open('light_open.png')
+        self.open_photo = ImageTk.PhotoImage(open_image)
+
         self.btn = tk.Button(self,image=self.close_photo,padx=50,pady=30,font=('arial',18),command=self.userClick)
         self.btn.pack(padx=50,pady=30)
         currentState = led.get()['led']
         if currentState:
-           self.btn.config(text="關")
+           self.btn.config(image=self.close_photo)
         else:
-           self.btn.config(text="開")
+           self.btn.config(image=self.open_photo)
     
     def userClick(self):
         currentState = led.get()['led']
         led.update({'led':not currentState})
         if currentState:
-           self.btn.config(text="開")
+           self.btn.config(image=self.open_photo)
         else:
-           self.btn.config(text="關")
+           self.btn.config(image=self.close_photo)
 
 
 def main():
