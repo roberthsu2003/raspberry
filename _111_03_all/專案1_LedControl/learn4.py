d0e2e013bbdaee36da97e6536936152570711404
roberthsu2003 <roberthsu2003@gmail.com>
2022-10-28 14:58:51 +0800

    畫圖

---
+++ _111_03_all/專案1_LedControl/learn4.py
@@ -0,0 +1,31 @@
+import tkinter as tk
+from PIL import Image,ImageTk
+
+class Window(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        image  = Image.open("light_open.png")
+        self.lightImage = ImageTk.PhotoImage(image)
+        print(image.size)
+        canvas = tk.Canvas(self,width=image.size[0]+20,height=image.size[1]+20)
+        canvas.create_image(10,10,anchor=tk.NW,image=self.lightImage)
+        canvas.delete("all")
+        image1  = Image.open("light_close.png")
+        self.lightImage1 = ImageTk.PhotoImage(image1)
+        canvas.create_image(10,10,anchor=tk.NW,image=self.lightImage1)
+
+        canvas.pack()
+        
+
+        
+        
+
+def main():
+    window = Window()
+    window.title("Canvas")
+    window.geometry("400x300+300+200")
+    window.mainloop()
+    
+
+if __name__ == "__main__":
+    main()
