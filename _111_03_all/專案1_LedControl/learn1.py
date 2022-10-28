c1dd64063bc92e8fcbd3f0da9a224ce225f0ae0d
roberthsu2003 <roberthsu2003@gmail.com>
2022-10-28 14:14:30 +0800

    畫線

--- _111_03_all/專案1_LedControl/learn1.py
+++ _111_03_all/專案1_LedControl/learn1.py
@@ -1,15 +1,21 @@
 import tkinter as tk
 
 class Window(tk.Tk):
-    def __init_(self):
+    def __init__(self):
         super().__init__()
-        pass
+        canvas1 = tk.Canvas(self,width=400,height=250,background="#7D532C")
+        canvas1.create_line(15, 30, 200, 30,fill="#FFBA84")
+        canvas1.create_line(300,35,300,200, dash=(4,2),fill="#FFBA84")
+        canvas1.create_line(55, 85, 155,85,105,180,55, 85,fill="#FFBA84")
+        canvas1.pack()
         
 
 def main():
     window = Window()
     window.title("Canvas")
     window.geometry("400x250+300+200")
+    window.mainloop()
+    
 
 if __name__ == "__main__":
     main()
