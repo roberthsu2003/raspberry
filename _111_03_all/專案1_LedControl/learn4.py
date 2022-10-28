9a769659ca3284e0357aa02a08cbb8e4bd7d626d
roberthsu2003 <roberthsu2003@gmail.com>
2022-10-28 15:19:00 +0800

    畫文字

--- _111_03_all/專案1_LedControl/learn4.py
+++ _111_03_all/專案1_LedControl/learn4.py
@@ -7,14 +7,20 @@ class Window(tk.Tk):
         image  = Image.open("light_open.png")
         self.lightImage = ImageTk.PhotoImage(image)
         print(image.size)
-        canvas = tk.Canvas(self,width=image.size[0]+20,height=image.size[1]+20)
+        canvas = tk.Canvas(self,width=image.size[0]+20,height=image.size[1]+40)
         canvas.create_image(10,10,anchor=tk.NW,image=self.lightImage)
         canvas.delete("all")
         image1  = Image.open("light_close.png")
         self.lightImage1 = ImageTk.PhotoImage(image1)
         canvas.create_image(10,10,anchor=tk.NW,image=self.lightImage1)
+        
 
+        #畫文字        
+        canvas.create_text(20,95,anchor=tk.NW,text="燈開",font=("sans_arial",16))
         canvas.pack()
+
+
+        
         
 
         
