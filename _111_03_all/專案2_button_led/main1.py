e038cbf373ad5f881ca997b91ad1885ffa501163
roberthsu2003 <roberthsu2003@gmail.com>
2022-10-31 15:11:02 +0800

    modify

--- _111_03_all/專案2_button_led/main1.py
+++ _111_03_all/專案2_button_led/main1.py
@@ -1,9 +1,15 @@
 from gpiozero import Button
+from signal import pause
+
+def say_hello():
+    print("Hello!")
+
+def say_goodbye():
+    print("Goodbye!")
 
 button = Button(18)
 
-while True:
-    if button.is_pressed:
-        print("Button is pressed")
-    else:
-        print("Buttonis not pressed")
+button.when_pressed = say_hello
+button.when_released = say_goodbye
+
+pause()
