68d42534869eee0f8aea7dc9881df0924200d19a
roberthsu2003 <roberthsu2003@gmail.com>
2022-10-31 16:02:53 +0800

    modify

--- _111_03_all/專案2_button_led/main2.py
+++ _111_03_all/專案2_button_led/main2.py
@@ -2,7 +2,18 @@ import tkinter as tk
 
 class Window(tk.Tk):
     def __init__(self):
-        super().__init__()
+        super().__init__()    
+        red = tk.Canvas(self,width=70,height=70)
+        red.create_rectangle(10,10,60,60,fill="red")
+        red.grid(row=0, column=0)
+
+        green = tk.Canvas(self,width=70,height=70)
+        green.create_rectangle(10,10,60,60,fill="green")
+        green.grid(row=0, column=1)
+
+        blue = tk.Canvas(self,width=70,height=70)
+        blue.create_rectangle(10,10,60,60,fill="blue")
+        blue.grid(row=0, column=2)
 
 def main():
     window = Window()
