90d9f02febb11d9fe6ada0b8e2e3bbc83363e3aa
roberthsu2003 <roberthsu2003@gmail.com>
2022-11-01 09:49:23 +0800

    modify

--- _111_03_all/專案2_button_led/main2.py
+++ _111_03_all/專案2_button_led/main2.py
@@ -1,19 +1,18 @@
 import tkinter as tk
 
 class ColorCanvas(tk.Canvas):
-    def __init__(self,parent,**kwargs):
+    def __init__(self,parent,rec_color,**kwargs):
         super().__init__(parent,**kwargs)
-    
+        self.rec_color = rec_color
+        space = 10
+        rec_width = int(self['width'])  - 2 * space 
+        rec_height = int(self['height']) - 2 * space   
+        self.create_rectangle(space, space, int(self['width']) - space, int(self['height']) - space,fill=self.rec_color)
 
 class Window(tk.Tk):
     def __init__(self):
         super().__init__()    
-        red = ColorCanvas(self,width=70,height=70,relief='ridge')
-        red.create_rectangle(10,10,60,60,fill="red")
-        x  = 70*1/5
-        y  = 70*4/6
-        width = 10
-        red.create_oval(x,y,x+10,y+10,fill="white",outline='white')
+        red = ColorCanvas(self,"red",width=70,height=70)
         red.bind('<ButtonRelease-1>',self.mouse_click)
         red.grid(row=0, column=0)
 
