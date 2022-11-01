c20c2abe92cd41596404bb3fb4ee8beb852d8562
roberthsu2003 <roberthsu2003@gmail.com>
2022-11-01 09:06:48 +0800

    新增圓點

--- _111_03_all/專案2_button_led/main2.py
+++ _111_03_all/專案2_button_led/main2.py
@@ -5,6 +5,10 @@ class Window(tk.Tk):
         super().__init__()    
         red = tk.Canvas(self,width=70,height=70,relief='ridge')
         red.create_rectangle(10,10,60,60,fill="red")
+        x  = 70*1/5
+        y  = 70*4/6
+        width = 10
+        red.create_oval(x,y,x+10,y+10,fill="white",outline='white')
         red.bind('<ButtonRelease-1>',self.mouse_click)
         red.grid(row=0, column=0)
 
@@ -18,10 +22,10 @@ class Window(tk.Tk):
 
     def mouse_click(self,event):
         print(event.__dict__)
-        event.widget.delete()
+        #event.widget.delete()
         event.widget.create_rectangle(10,10,60,60,fill='white')
         event.widget.create_rectangle(20,20,50,50,fill='red')
-        event.widget.update()
+        #event.widget.update()
 
 def main():
     window = Window()
