764884193ae993ab6fe6fcec76a9fa5dc38ee44c
roberthsu2003 <roberthsu2003@gmail.com>
2022-11-01 10:56:06 +0800

    modify

--- _111_03_all/專案2_button_led/main2.py
+++ _111_03_all/專案2_button_led/main2.py
@@ -1,12 +1,16 @@
 import tkinter as tk
 
 class ColorCanvas(tk.Canvas):
+    #建立Class的propertuy
+    #建立一個Class內建的常數
+    ON =  True
+    OFF = False
     def __init__(self,parent,rec_color,**kwargs):
         self.width = kwargs['width']
         self.height = kwargs['height']
         super().__init__(parent,**kwargs)
         self.rec_color = rec_color
-        self.__state = False
+        self.__state = ColorCanvas.OFF
         self.space = self.width / 7
         rec_width = self.width  - 2 * self.space 
         rec_height = self.height - 2 * self.space   
@@ -31,12 +35,11 @@ class ColorCanvas(tk.Canvas):
 
 class Window(tk.Tk):
     def __init__(self):
-        super().__init__()
-           
+        super().__init__()           
         red = ColorCanvas(self,"red",width=100,height=100)
         red.bind('<ButtonRelease-1>',self.mouse_click)
         red.grid(row=0, column=0)
-        red.state = True
+        red.state = ColorCanvas.ON
         print(f"red狀態:{red.state}")
         green = ColorCanvas(self,"green",width=100,height=100)        
         green.grid(row=0, column=1)
