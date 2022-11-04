4e77463a1187db7f1064fedd737f2cd0d06be9f6
roberthsu2003 <roberthsu2003@gmail.com>
2022-11-04 16:01:09 +0800

    modify

--- _111_03_all/專案3_溫度光線/main.py
+++ _111_03_all/專案3_溫度光線/main.py
@@ -5,9 +5,7 @@ from tools import data
 
 class Window(tk.Tk):
     def __init__(self):
-        super().__init__()
-        
-        
+        super().__init__()   
         self.label = tk.Label(self,text="",font=("arial",30))
         self.label.pack(padx=50,pady=30)
         self.change_time()
