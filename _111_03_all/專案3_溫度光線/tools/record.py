fdc35f3ce72e3378b34882d8cfbc54e7b6cbd054
roberthsu2003 <roberthsu2003@gmail.com>
2022-11-08 10:11:39 +0800

    modify

--- _111_03_all/專案3_溫度光線/tools/record.py
+++ _111_03_all/專案3_溫度光線/tools/record.py
@@ -1,9 +1,15 @@
 import os
+from datetime import datetime
 
 directory = os.path.abspath("./record")
 if not os.path.isdir(directory):
     os.makedirs(directory)
 
 def recordData(distance,lightValue):
-    print("記錄")
+    current = datetime.now()
+    current_date = current.date()
+    filename = current_date.strftime("%Y-%m-%d.csv")    
+    currentFiles = os.listdir(directory)
+    if filename not in currentFiles:
+        print(f"沒有{filename}此檔")
     
