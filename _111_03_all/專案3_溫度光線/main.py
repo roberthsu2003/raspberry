e41a9d9723dfbde70464cd6ae97d504bd20029ab
roberthsu2003 <roberthsu2003@gmail.com>
2022-11-08 11:39:58 +0800

    modify

--- _111_03_all/專案3_溫度光線/main.py
+++ _111_03_all/專案3_溫度光線/main.py
@@ -1,17 +1,23 @@
 import tkinter as tk
+from tkinter import ttk
 from datetime import datetime
 from tools import data,record
 
 
-
+class CustomView(ttk.Treeview):
+    def __init__(self,master,**kwargs):
+        super().__init__(master,**kwargs)
+    
 class Window(tk.Tk):
     def __init__(self):
         super().__init__()   
         self.label = tk.Label(self,text="",font=("arial",30))
         self.label.pack(padx=50,pady=30)
+        self.customView = CustomView(self)
+        self.customView.pack()
+
         self.change_time()
-        self.window_time()
-        
+        self.window_time()      
 
         
     def change_time(self):
