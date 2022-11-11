a9e8f0e7fabc2f406e56fa35b1080fc4355eebbe
roberthsu2003 <roberthsu2003@gmail.com>
2022-11-11 14:50:17 +0800

    modify

--- _111_03_all/專案3_溫度光線/tools/record.py
+++ _111_03_all/專案3_溫度光線/tools/record.py
@@ -48,6 +48,13 @@ def recordData(distance,lightValue,absolute_path):
         cred = credentials.Certificate(full_path_key)
         app = firebase_admin.initialize_app(cred)
         db = firestore.client()
+    
+    data = {
+        '日期':current.strftime("%Y-%m-%d %H:%M:%S"),
+        '距離':distance,
+        '亮度':lightValue
+    }
+    db.collection('records').document(current.strftime("%Y%m%d%H%M%S")).set(data)
 
 def getData():
     with open(full_path_csvFile,"r",newline='') as file:
