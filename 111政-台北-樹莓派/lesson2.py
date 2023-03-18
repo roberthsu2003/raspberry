b8006cf6af484c821dcbf7ea8f114b8824d58aa5
roberthsu2003 <roberthsu2003@gmail.com>
2023-03-18 14:31:43 +0800

    modify

--- 111政-台北-樹莓派/lesson2.py
+++ 111政-台北-樹莓派/lesson2.py
@@ -4,8 +4,8 @@ class Window(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("按鈕")
-        hello_button = tk.Button(self, text="Say Hello", command=self.say_hello)
-        hello_button.pack(side=tk.LEFT, padx=(20, 0), pady=(0, 20))
+        hello_button = tk.Button(self, text="Say Hello", command=self.say_hello,width=30, height=10)
+        hello_button.pack(side=tk.LEFT, padx=(20, 20), pady=(20, 20))
 
     def say_hello(self):
         print("Hello! Robert")
