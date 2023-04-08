b7605d0d7fec43f1fddefc2cfd3f93c8d642ef7d
roberthsu2003 <roberthsu2003@gmail.com>
2023-04-08 11:42:02 +0800

    modify

--- 111政-台北-樹莓派/lesson3.py
+++ 111政-台北-樹莓派/lesson3.py
@@ -1,3 +1,8 @@
 import private
+import requests
+
 url = f'https://maker.ifttt.com/trigger/button_press/with/key/{private.iftttKey}?value1=31c&value2=51'
-print(url)
+
+r = requests.get(url)
+if r.status_code == 200:
+    print("發送成功")
