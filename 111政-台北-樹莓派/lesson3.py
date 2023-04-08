4f261727e764db65d2df5b88ef5819b83e93cfab
roberthsu2003 <roberthsu2003@gmail.com>
2023-04-08 13:57:08 +0800

    modify

--- 111政-台北-樹莓派/lesson3.py
+++ 111政-台北-樹莓派/lesson3.py
@@ -1,8 +1,20 @@
 import private
 import requests
 
-url = f'https://maker.ifttt.com/trigger/button_press/with/key/{private.iftttKey}?value1=31c&value2=51'
+#url = f'https://maker.ifttt.com/trigger/button_press/with/key/{private.iftttKey}?value1=31c&value2=51'
 
-r = requests.get(url)
-if r.status_code == 200:
-    print("發送成功")
+#r = requests.get(url)
+#if r.status_code == 200:
+#    print("發送成功")
+
+from gpiozero import Button
+from signal import pause
+
+def say_hello():
+    print("Hello!")
+
+button = Button(18)
+
+button.when_pressed = say_hello
+
+pause()
