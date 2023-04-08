a15f6547854745a691c129e348e7f5ba7f8349e2
roberthsu2003 <roberthsu2003@gmail.com>
2023-04-08 14:47:30 +0800

    modify

--- 111政-台北-樹莓派/lesson3.py
+++ 111政-台北-樹莓派/lesson3.py
@@ -9,6 +9,7 @@ import requests
 
 from gpiozero import Button
 from signal import pause
+from gpiozero import RGBLED
 
 state = False
 
@@ -17,10 +18,13 @@ def user_press():
     state = not state
     if state == True:
         print("開燈")
+        led.color=(1,0,0)
     else:
         print("關燈")
+        led.color=(0,0,0)
 
 button = Button(18)
+led = RGBLED(red=17, green=27, blue=22)
 
 button.when_pressed = user_press
 
