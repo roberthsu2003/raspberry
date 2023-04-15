a955f31669fad752d6d215f3ca51e9bf9158e047
roberthsu2003 <roberthsu2003@gmail.com>
2023-04-15 11:39:02 +0800

    modify

--- 111政-台北-樹莓派/lesson4_1.py
+++ 111政-台北-樹莓派/lesson4_1.py
@@ -1,9 +1,15 @@
 import gpiozero
 from time import sleep
+
 mcp3008 = gpiozero.MCP3008(channel=7)
+buzzer = gpiozero.Buzzer(25)
 
 while(True):    
     lightValue = round(mcp3008.value*1000)
     print(lightValue)
+    if lightValue < 40:
+        buzzer.on()
+    else:
+        buzzer.off()
     sleep(1)
 
