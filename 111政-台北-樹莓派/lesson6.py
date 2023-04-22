e3bef2eb2333a336a28cb4dbaebb57021f1e471e
Roberthsu2003 <roberthsu2003@gmail.com>
2023-04-22 14:26:26 +0800

    modify

--- 111政-台北-樹莓派/lesson6.py
+++ 111政-台北-樹莓派/lesson6.py
@@ -1,6 +1,14 @@
 
 import firebase_admin
 from firebase_admin import credentials
+from firebase_admin import db
 
 cred = credentials.Certificate("raspberrytest-51322-firebase-adminsdk-qdsvd-6ec84a9325.json")
-firebase_admin.initialize_app(cred)
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://raspberrytest-51322-default-rtdb.asia-southeast1.firebasedatabase.app/'
+})
+ref = db.reference('/')
+print(ref.get())
+
+
+
