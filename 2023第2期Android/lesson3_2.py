82770750f21094e374092134142b3010d4c40780
roberthsu2003 <roberthsu2003@gmail.com>
2023-06-13 15:59:43 +0800

    modify

--- 2023第2期Android/lesson3_2.py
+++ 2023第2期Android/lesson3_2.py
@@ -1,27 +1,50 @@
 import tkinter as tk
 from tkinter import ttk
+from gpiozero import LED
+#import pprint
 
-class Window(tk.Tk):
+class Window(tk.Tk):    
     def __init__(self):
         super().__init__()
+        self.state = False
         self.title('這是我的第一個視窗')
         self.resizable(False, False)
         s = ttk.Style()
         s.theme_use('clam')
-        s.configure('Title.TLabel',foreground='red',background='yellow',font=('Arial',20))
-        s.configure('Led.TButton',foreground='red',background='yellow',font=('Arial',20),borderwidth=5,padding=(10,20))
-        print(s.layout('TButton'))
+        s.configure('Title.TLabel',font=('Arial',20))
+        s.configure('LEDClose.TButton',                    
+                    font=('Arial',20),
+                    borderwidth=5,
+                    padding=(10,20),
+                    )
+        s.configure('LEDOpen.TButton',
+                    background='yellow',                    
+                    font=('Arial',20),
+                    borderwidth=5,
+                    padding=(10,20),
+                    )
+        #pprint.pprint(s.layout('TButton'))
+        #pprint.pprint(s.element_options('Button.focus'))
         title_label = ttk.Label(self,text="LED控制器",style='Title.TLabel')        
         title_label.pack(pady=25,padx=100)
 
-        led_btn = ttk.Button(self,text="LED 開",style='Led.TButton',command=self.user_click)
-             
-        led_btn.pack(pady=(10,50))
+        self.led_btn = ttk.Button(self,text="LED 開",style='LEDClose.TButton',command=self.user_click)             
+        self.led_btn.pack(pady=(10,50))
 
     def user_click(self):
-        print('user click')
+        self.state = not self.state
+        if self.state:
+            self.led_btn.configure(text='LED 關')
+            self.led_btn.configure(style='LEDOpen.TButton')
+            led.on()
+        else:
+            self.led_btn.configure(text='LED 開')
+            self.led_btn.configure(style='LEDClose.TButton')
+            led.off()
 
 if __name__ == "__main__":
+    led = LED(23)
+    led.off()
     window = Window()   
     window.mainloop()
    
