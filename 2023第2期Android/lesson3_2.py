a27ef4a54e13c232fdd66084b1afd4cb40b6124c
roberthsu2003 <roberthsu2003@gmail.com>
2023-06-13 11:30:03 +0800

    modify

--- 2023第2期Android/lesson3_2.py
+++ 2023第2期Android/lesson3_2.py
@@ -7,14 +7,18 @@ class Window(tk.Tk):
         self.title('這是我的第一個視窗')
         self.resizable(False, False)
         s = ttk.Style()
-        print(s.theme_names())
-        print(s.theme_use())
         s.theme_use('clam')
-        title_label = ttk.Label(self,text="LED控制器",font=('Helvetica', '16'))
+        s.configure('Title.TLabel',foreground='red',background='yellow',font=('Arial',20))
+        s.configure('Led.TButton',foreground='red',background='yellow',font=('Arial',20))
+        print(s.layout('TButton'))
+        title_label = ttk.Label(self,text="LED控制器",style='Title.TLabel')        
         title_label.pack(pady=25,padx=100)
 
-        led_btn = ttk.Button(self,text="LED 開")
+        led_btn = ttk.Button(self,text="LED 開",style='Led.TButton',command=self.user_click)
         led_btn.pack(pady=(10,50))
+
+    def user_click(self):
+        print('user click')
 
 if __name__ == "__main__":
     window = Window()   
