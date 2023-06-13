4e3e4c60f119ab6adec6734ff1ad9652dc398850
roberthsu2003 <roberthsu2003@gmail.com>
2023-06-13 10:47:41 +0800

    modify

--- 2023第2期Android/lesson3_2.py
+++ 2023第2期Android/lesson3_2.py
@@ -1,14 +1,19 @@
 import tkinter as tk
+from tkinter import ttk
 
 class Window(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title('這是我的第一個視窗')
-        self.resizable(False, False)        
-        title_label = tk.Label(self,text="LED控制器",font=('Helvetica', '16'),bg='#ff0000')
+        self.resizable(False, False)
+        s = ttk.Style()
+        print(s.theme_names())
+        print(s.theme_use())
+        s.theme_use('clam')
+        title_label = ttk.Label(self,text="LED控制器",font=('Helvetica', '16'))
         title_label.pack(pady=25,padx=100)
 
-        led_btn = tk.Button(self,text="LED 開",font=('Helvetica', '16'),padx=20,pady=20)
+        led_btn = ttk.Button(self,text="LED 開")
         led_btn.pack(pady=(10,50))
 
 if __name__ == "__main__":
