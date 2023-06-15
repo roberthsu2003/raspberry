f155291e6f003c5bf53f87ac1a52c503036d7c99
roberthsu2003 <roberthsu2003@gmail.com>
2023-06-15 11:56:14 +0800

    modify

--- 2023第2期Android/lesson3_2.py
+++ 2023第2期Android/lesson3_2.py
@@ -7,21 +7,10 @@ class LEDButton(ttk.Button):
     def __init__(self,master,led,**kwargs):
         super().__init__(master,**kwargs)
         self.led = led
-    
-
-class Window(tk.Tk):    
-    def __init__(self,redLed,**kwargs):
-        '''
-        @parmater redLed,是gpiozeor.LED的實體
-        '''
-        super().__init__(**kwargs)
-        self.redLed = redLed
         self.state = False
-        self.title('這是我的第一個視窗')
-        self.resizable(False, False)
+        self.configure(command=self.user_click)
         s = ttk.Style()
         s.theme_use('clam')
-        s.configure('Title.TLabel',font=('Arial',20))
         s.configure('LEDClose.TButton',                    
                     font=('Arial',20),
                     borderwidth=5,
@@ -33,24 +22,40 @@ class Window(tk.Tk):
                     borderwidth=5,
                     padding=(10,20),
                     )
+    
+    def user_click(self):
+        self.state = not self.state
+        if self.state:
+            self.configure(text='LED 關')
+            self.configure(style='LEDOpen.TButton')
+            self.led.on()
+        else:
+            self.configure(text='LED 開')
+            self.configure(style='LEDClose.TButton')
+            self.led.off()
+    
+
+class Window(tk.Tk):    
+    def __init__(self,redLed,**kwargs):
+        '''
+        @parmater redLed,是gpiozeor.LED的實體
+        '''
+        super().__init__(**kwargs)       
+        
+        self.title('這是我的第一個視窗')
+        self.resizable(False, False)
+        s = ttk.Style()
+        s.theme_use('clam')
+        s.configure('Title.TLabel',font=('Arial',20))        
         #pprint.pprint(s.layout('TButton'))
         #pprint.pprint(s.element_options('Button.focus'))
         title_label = ttk.Label(self,text="LED控制器",style='Title.TLabel')        
         title_label.pack(pady=25,padx=100)
 
-        self.led_btn = ttk.Button(self,text="LED 開",style='LEDClose.TButton',command=self.user_click)             
+        self.led_btn = LEDButton(self,led=redLed,text="LED 開",style='LEDClose.TButton')             
         self.led_btn.pack(pady=(10,50))
 
-    def user_click(self):
-        self.state = not self.state
-        if self.state:
-            self.led_btn.configure(text='LED 關')
-            self.led_btn.configure(style='LEDOpen.TButton')
-            self.redLed.on()
-        else:
-            self.led_btn.configure(text='LED 開')
-            self.led_btn.configure(style='LEDClose.TButton')
-            self.redLed.off()
+    
 
 
 
