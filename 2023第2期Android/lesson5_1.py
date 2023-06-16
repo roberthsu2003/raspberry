fece6e51a9521cea2deb5d7f4b82ced1721daff5
roberthsu2003 <roberthsu2003@gmail.com>
2023-06-16 11:16:20 +0800

    modify

--- 2023第2期Android/lesson5_1.py
+++ 2023第2期Android/lesson5_1.py
@@ -7,15 +7,24 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(17, GPIO.OUT)# set GPIO 17 as output for white led  
 GPIO.setup(27, GPIO.OUT)# set GPIO 27 as output for red led  
 GPIO.setup(22, GPIO.OUT)# set GPIO 22 as output for red led
-blue = GPIO.PWM(17, 70)
-i = 0
+blue = GPIO.PWM(17, 75)
+green = GPIO.PWM(27, 75)
+red = GPIO.PWM(22, 75)   
+
+i = 75
 while(True): 
-    if(i<70):
-        i += 1 
-    else:
-        i = 0 
+    red.start(i)
+    sleep(0.5)
+    red.stop()
+    sleep(0.5)
+
     blue.start(i)
     sleep(0.5)
     blue.stop()
+    sleep(0.5)
+    
+    green.start(i)
+    sleep(0.5)
+    green.stop()
     sleep(0.5)
 GPIO.cleanup() 
