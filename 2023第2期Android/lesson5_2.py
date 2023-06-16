faf6ef127e59623a864d33e53e6abf0dce555252
roberthsu2003 <roberthsu2003@gmail.com>
2023-06-16 16:24:10 +0800

    modify

--- 2023第2期Android/lesson5_2.py
+++ 2023第2期Android/lesson5_2.py
@@ -1,8 +1,16 @@
-from gpiozero import Button
+from gpiozero import Button,RGBLED
 from signal import pause
+from random import randint
+
 button = Button(18)
+led = RGBLED(red=17, green=27, blue=22);
 def user_press():
-    print("User pressed")
+    r = randint(0,100) / 100;
+    g = randint(0,100) / 100;
+    b = randint(0,100) / 100;
+    print("R={},G={},B={}".format(r,g,b));
+    led.color = (r, g, b);
+    
 
 button.when_pressed = user_press;
 pause()
