2b595228d9eb07dd6ceed317a4bdf576c5ad58a6
roberthsu2003 <roberthsu2003@gmail.com>
2023-06-16 14:02:26 +0800

    modify

---
+++ 2023第2期Android/rgbLed.py
@@ -0,0 +1,17 @@
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+
+class RGBLed():
+    def __init__(self, red_pin, green_pin, blue_pin):        
+        GPIO.setup(red_pin, GPIO.OUT)
+        GPIO.setup(green_pin, GPIO.OUT)
+        GPIO.setup(blue_pin, GPIO.OUT)
+        self.red = GPIO.PWM(red_pin, 75)
+        self.green = GPIO.PWM(green_pin, 75)
+        self.blue = GPIO.PWM(blue_pin, 75)
+
+    def close(self):
+        self.red.stop()
+        self.blue.stop()
+        self.green.stop()
+        GPIO.cleanup()
