ab19bd2b58111ec89a8240db0abb51c3725df712
roberthsu2003 <roberthsu2003@gmail.com>
2023-06-16 11:35:09 +0800

    modify

--- 2023第2期Android/lesson5_1.py
+++ 2023第2期Android/lesson5_1.py
@@ -12,19 +12,26 @@ green = GPIO.PWM(27, 75)
 red = GPIO.PWM(22, 75)   
 
 i = 75
-while(True): 
-    red.start(i)
-    sleep(0.5)
-    red.stop()
-    sleep(0.5)
-
-    blue.start(i)
-    sleep(0.5)
-    blue.stop()
-    sleep(0.5)
+try:
+    while(True):
     
-    green.start(i)
-    sleep(0.5)
-    green.stop()
-    sleep(0.5)
-GPIO.cleanup() 
+        red.start(i)
+        sleep(0.5)
+        red.stop()
+        sleep(0.5)
+
+        blue.start(i)
+        sleep(0.5)
+        blue.stop()
+        sleep(0.5)
+        
+        green.start(i)
+        sleep(0.5)
+        green.stop()
+        sleep(0.5)
+except KeyboardInterrupt:
+        blue.stop()
+        red.stop()
+        green.stop()
+        GPIO.cleanup() 
+        print("應用程式結束")
