31736aaab4ed2e83887b1529f9fbce6a0b595d13
roberthsu2003 <roberthsu2003@gmail.com>
2023-06-19 11:58:19 +0800

    modify

--- 2023第2期Android/lesson6_1.py
+++ 2023第2期Android/lesson6_1.py
@@ -5,15 +5,18 @@ from time import sleep
 
 
 if __name__ == "__main__":
-    mcp3008 = zero.MCP3008(channel=7)
+    mcp3008_ch7 = zero.MCP3008(channel=7)
+    mcp3008_ch6 = zero.MCP3008(channel=6)
     try:
         while True:
-            value = round(mcp3008.value*100)
+            value = round(mcp3008_ch7.value*100)
             print("光敏電阻値: ", value)
             if value > 20:
                 print("光線亮")
             else:
-                print("光線暗")           
+                print("光線暗") 
+
+            print("LM35",mcp3008_ch6.value*100*3.3)    
 
             sleep(1)
     except KeyboardInterrupt:
