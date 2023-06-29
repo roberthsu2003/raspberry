88658bc065b59a350809b91b0b0b5997ea5d5885
roberthsu2003 <roberthsu2003@gmail.com>
2023-06-29 11:37:50 +0800

    modify

--- 2023第2期Android/lesson6_1.py
+++ 2023第2期Android/lesson6_1.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
 
             datetimeStr = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-            response = requests.get(f'https://roberthsu2003-cautious-journey-5gw7r7vwjw37jrr-8000.preview.app.github.dev/raspberry?time={datetimeStr}&light={value}&temperature={mcp3008_ch6.value}')
+            response = requests.get(f'https://fastapi-g07j.onrender.com/raspberry?time={datetimeStr}&light={value}&temperature={mcp3008_ch6.value * 1000}')
             
             if response.ok:
                 print("上傳資料成功")
